600f7d01fbdbf09d459c89b3c63a32cb8c897860
Nebelwerfergranate <acht-acht@mail.ru>
2015-08-29 23:16:11 +0300

    Реализован метод "быстрой сортировки" по возрастанию для массивов целочисленных значений.

--- qSort/Program.cs
+++ qSort/Program.cs
@@ -6,10 +6,61 @@ using System.Threading.Tasks;
 
 namespace qSort
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            int[] myInts = { 0, 2, 4, 6, 8, 1, 3, 5, 7, 9 };
+            Sorter.QSortAsc(myInts);
+            foreach (int value in myInts)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    static class Sorter
+    {
+        public static int[] QSortAsc(int[] arr)
+        {
+            QuickSortAscending(ref arr, 0, arr.Length-1);
+            return arr;
+        }
+        private static void QuickSortAscending(ref int[] arr, int left, int right)
+        {
+            int currentLeft = left;
+            int currentRight = right;
+            int temp = 0;
+            int middle = arr[(currentLeft + currentRight) / 2];
+
+            do
+            {
+                while (arr[currentLeft] < middle)
+                {
+                    currentLeft++;
+                }
+                while (arr[currentRight] > middle)
+                {
+                    currentRight--;
+                }
+                if (currentLeft <= currentRight)
+                {
+                    temp = arr[currentLeft];
+                    arr[currentLeft] = arr[currentRight];
+                    arr[currentRight] = temp;
+                    currentLeft++;
+                    currentRight--;
+                }
+            } while (currentLeft <= currentRight);
+            if (currentRight > left)
+            {
+                QuickSortAscending(ref arr, left, currentRight);
+            }
+            if (currentLeft < right)
+            {
+                QuickSortAscending(ref arr, currentLeft, right);
+            }
         }
     }
 }
